fa75332340dfa675af68b1f7307be9af75f6d390
Mayank Bisht <bishtmayank799@gmail.com>
2021-10-24 16:00:24 +0530

    created searchBar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-
+import React, { useEffect, Fragment } from 'react';
+import SearchBar from './components/layout/SearchBar';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
@@ -11,9 +11,9 @@ const App = () => {
 		M.AutoInit();
 	});
 	return (
-		<div className='App'>
-			<h1>Hello React</h1>
-		</div>
+		<Fragment>
+			<SearchBar />
+		</Fragment>
 	);
 };
 
