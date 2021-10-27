1ad26d1272b13f0857cdeda3e9af21acdc027142
Mayank Bisht <bishtmayank799@gmail.com>
2021-10-27 18:56:47 +0530

    redux setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, Fragment } from 'react';
+import './App.css';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
@@ -10,7 +11,8 @@ import TechListModal from './components/techs/TechListModal';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
 	useEffect(() => {
@@ -18,17 +20,19 @@ const App = () => {
 		M.AutoInit();
 	});
 	return (
-		<Fragment>
-			<SearchBar />
-			<div className='container'>
-				<AddBtn />
-				<AddLogModal />
-				<EditLogModal />
-				<AddTechModal />
-				<TechListModal />
-				<Logs />
-			</div>
-		</Fragment>
+		<Provider store={store}>
+			<Fragment>
+				<SearchBar />
+				<div className='container'>
+					<AddBtn />
+					<AddLogModal />
+					<EditLogModal />
+					<AddTechModal />
+					<TechListModal />
+					<Logs />
+				</div>
+			</Fragment>
+		</Provider>
 	);
 };
 
