36a8cd3637de81d3d965f877d1b23adb4d76eb30
Mayank Bisht <bishtmayank799@gmail.com>
2021-10-30 19:33:30 +0530

    created deleteLog functionality

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
+import { connect } from 'react-redux';
+import { deleteLog } from '../../actions/logActions';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
-const LogItem = ({ log }) => {
+const LogItem = ({ log, deleteLog }) => {
+	const onDelete = () => {
+		deleteLog(log.id);
+		M.toast({
+			html: 'Log Successfully Deleted',
+			displayLength: 2500,
+			classes: 'orange',
+		});
+	};
+
 	return (
 		<li className='collection-item'>
 			<div>
@@ -27,7 +39,7 @@ const LogItem = ({ log }) => {
 						{log.date}
 					</Moment>
 				</span>
-				<a href='#!' className='secondary-content'>
+				<a href='#!' onClick={onDelete} className='secondary-content'>
 					<i className='material-icons grey-text'>delete</i>
 				</a>
 			</div>
@@ -37,6 +49,7 @@ const LogItem = ({ log }) => {
 
 LogItem.propTypes = {
 	log: PropTypes.object.isRequired,
+	deleteLog: PropTypes.func.isRequired,
 };
 
-export default LogItem;
+export default connect(null, { deleteLog })(LogItem);
